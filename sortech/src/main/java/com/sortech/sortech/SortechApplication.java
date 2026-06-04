beab97513b8d74d81fc7c4ee1ed1cece1ca7482f
ilkdy1234567-ui <ilkdy1234567@gmail.com>
2026-06-04 12:21:02 +0900

    초기파일

---
+++ sortech/src/main/java/com/sortech/sortech/SortechApplication.java
@@ -0,0 +1,13 @@
+package com.sortech.sortech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortechApplication.class, args);
+	}
+
+}
---
+++ sortech/src/test/java/com/sortech/sortech/SortechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sortech.sortech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
